384b766a9b5488c4a27f8b2271fa243cbee0d492
smBaay2201 <bayufirdaus2201@gmail.com>
2025-05-05 23:37:14 +0700

    tesss

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="/src/styles.css" rel="stylesheet">
-    <title>Login Page</title>
+    <title>Login halaman</title>
     <link href="./output.css" rel="stylesheet">
 </head>
 <body>
